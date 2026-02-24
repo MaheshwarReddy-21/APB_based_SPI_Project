6b6bf19733213ecc4f60f03677e000efd2275904
Maheswar Reddy <chowdurumaheswarreddy@gmail.com>
2026-02-24 22:05:47 +0530

    Remove copyright header from spi_intf.sv
    Removed copyright notice and header comments from spi_intf.sv.

--- APB_based_SPI_Project/rtl/spi_intf.sv
+++ APB_based_SPI_Project/rtl/spi_intf.sv
@@ -1,23 +1,4 @@
 
-/********************************************************************************************
-
-Copyright 2024 - Maven Silicon Softech Pvt Ltd.  
-www.maven-silicon.com
-
-All Rights Reserved.
-
-This source code is an unpublished work belongs to Maven Silicon Softech Pvt Ltd.
-It is not to be shared with or used by any third parties who have not enrolled for our paid 
-training courses or received any written authorization from Maven Silicon.
-
-Filename                :       spi_intf.sv   
-
-module Name             :       spi intf
-
-Description             :       SPI Interface for SPI Core Testbench
-
-
-*********************************************************************************************/
   interface spi_intf(input bit clock);//In SPI, DUT owns the clock (SCLK) → testbench just listens → no assignment needed
    logic ss;
    logic sclk;
