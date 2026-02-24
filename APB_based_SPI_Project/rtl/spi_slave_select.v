e9fc39e91bb66b112ce6e55eb120834a0322c861
Maheswar Reddy <chowdurumaheswarreddy@gmail.com>
2026-02-24 22:06:04 +0530

    Clean up comments in spi_slave_select.v
    Removed copyright notice and module description comments.

--- APB_based_SPI_Project/rtl/spi_slave_select.v
+++ APB_based_SPI_Project/rtl/spi_slave_select.v
@@ -1,23 +1,4 @@
 
-/********************************************************************************************
-
-Copyright 2024 - Maven Silicon Softech Pvt Ltd.  
-www.maven-silicon.com
-
-All Rights Reserved.
-
-This source code is an unpublished work belongs to Maven Silicon Softech Pvt Ltd.
-It is not to be shared with or used by any third parties who have not enrolled for our paid 
-training courses or received any written authorization from Maven Silicon.
-
-Filename                :       spi_slave_select.v   
-
-module Name             :       spi slave select
-
-Description             :       spi slave select module for APB based SPI Core Design
-
-
-*********************************************************************************************/
 
  module spi_slave_select(input PRESETn,
  		         input [1:0] spi_mode,
