276502801474f5a28414dee2703edba23cce0c3c
Maheswar Reddy <chowdurumaheswarreddy@gmail.com>
2026-02-24 22:04:34 +0530

    Clean up comments in apb_slave.v
    Removed copyright notice and description comments.

--- APB_based_SPI_Project/rtl/apb_slave.v
+++ APB_based_SPI_Project/rtl/apb_slave.v
@@ -1,23 +1,5 @@
 
-/********************************************************************************************
 
-Copyright 2024 - Maven Silicon Softech Pvt Ltd.  
-www.maven-silicon.com
-
-All Rights Reserved.
-
-This source code is an unpublished work belongs to Maven Silicon Softech Pvt Ltd.
-It is not to be shared with or used by any third parties who have not enrolled for our paid 
-training courses or received any written authorization from Maven Silicon.
-
-Filename                :       apb_slave.v   
-
-module Name             :       apb_slave
-
-Description             :       APB Slave Interface for APB based SPI Core Design
-
-
-*********************************************************************************************/
  
 
  
