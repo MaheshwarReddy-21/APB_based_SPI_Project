b8797745f9c5af1133c0f2abada45b49af77a079
Maheswar Reddy <chowdurumaheswarreddy@gmail.com>
2026-02-24 22:04:14 +0530

    Remove copyright and header comments from apb_intf.sv
    Removed copyright notice and header comments from apb_intf.sv.

--- APB_based_SPI_Project/rtl/apb_intf.sv
+++ APB_based_SPI_Project/rtl/apb_intf.sv
@@ -1,23 +1,4 @@
 
-/********************************************************************************************
-
-Copyright 2024 - Maven Silicon Softech Pvt Ltd.  
-www.maven-silicon.com
-
-All Rights Reserved.
-
-This source code is an unpublished work belongs to Maven Silicon Softech Pvt Ltd.
-It is not to be shared with or used by any third parties who have not enrolled for our paid 
-training courses or received any written authorization from Maven Silicon.
-
-Filename                :       apb_intf.sv   
-
-module Name             :       apb intf
-
-Description             :       APB Interface for SPI Core Testbench
-
-
-*********************************************************************************************/
 
  interface apb_intf(input bit clock);
    bit PCLK;
